ed1d3444d106a2b6dfd6750a5808b08e7181ff72
Hallory <khalpeep@gmail.com>
2023-07-02 16:46:41 +0200

    added changes

--- homework4/main.js
+++ homework4/main.js
@@ -5,33 +5,47 @@ const marks = [4, 5, 5, 3, 4, 5];
 let women 
 let men
 let arr = []
-for (let i = 0; i < students.length; i++) {
-    const student = students[i];
-        if(student.charAt(student.length-1)=='а'||student.charAt(student.length-1)=='я'){
-            women = students.filter(sort=>{return (sort.charAt(sort.length-1)=='а')})
-           
-        }
-        else {
-            men = students.filter(sort=>{return sort.charAt(sort.length-1)!='а'})
+function getPair(students){
+
+    for (let i = 0; i < students.length; i++) {
+        const student = students[i];
+            if(student.charAt(student.length-1)=='а'||student.charAt(student.length-1)=='я'){
+                women = students.filter(sort=>{return (sort.charAt(sort.length-1)=='а')})
+               
+            }
+            else {
+                men = students.filter(sort=>{return sort.charAt(sort.length-1)!='а'})
+                
+            }
             
-        }
-        
-        
-        
+            
+            
+    }
+     console.log(women)
+     console.log(men)
+     
 }
- console.log(women)
- console.log(men)
 
- const pairs = [];
+getPair(students)
+let pairs = [];
+function getThemes(pairs){
+    
 
- for (let i = 0; i < men.length; i++) {
-    const pair = [men[i]+" i "+ women[i]+" thema:"+themes[i]];
-    pairs.push(pair);
-  }
-console.log(pairs)
-
-for (let i = 0; i < students.length; i++) {
-    students[i] = [students[i],marks[i]]
-    console.log(students[i])
-
+     for (let i = 0; i < men.length; i++) {
+        const pair = [men[i]+" i "+ women[i]+" thema:"+themes[i]];
+        pairs.push(pair);
+      }
+    return(pairs)
 }
+console.log(getThemes(pairs))
+ 
+
+function getMark(students,marks){
+    for (let i = 0; i < students.length; i++) {
+        students[i] = [students[i],marks[i]]
+        console.log(students[i])
+        
+    }
+}
+getMark(students, marks)
+
