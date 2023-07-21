1bc9728d288153bebd82082b2896d7b8f3585603
Hallory <khalpeep@gmail.com>
2023-07-21 17:18:43 +0200

    added 9 homework

---
+++ homework9/main.js
@@ -0,0 +1,62 @@
+
+function getRandomColor(){
+    var letters = "0123456789ABCDEF"
+    var color = "#";
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random()*16)];
+        
+    }
+    return color;
+}
+
+function generateBlocks() {
+  var container = document.querySelector(".container");
+
+  var maxBox = 25;
+
+  var maxBoxPerLine = 5;
+
+  container.innerHTML = "";
+
+  for (let i = 0; i < maxBox; i++) {
+    var newBox = document.createElement("div");
+    newBox.className = "my-block";
+    newBox.textContent = "Block" + (i + 1);
+    var randomColor = getRandomColor();
+    newBox.style.backgroundColor = randomColor
+    container.appendChild(newBox);
+    if ((i + 1) % maxBoxPerLine === 0) {
+      container.appendChild(document.createElement("br"));
+    }
+  }
+}
+
+setInterval(generateBlocks, 1000)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
