e21a13f638e09760b72b89f7555ff6c8ebefc9a5
Hallory <khalpeep@gmail.com>
2023-07-29 18:30:55 +0200

    added 16 homework

---
+++ homework16/homework16-app/src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css';
+import Post from './post/Post';
+
+function App() {
+  const ANAKIN_IMAGE = 'https://media1.popsugar-assets.com/files/thumbor/_UDLEp7pQ-Uip4ow4R2SqdKL100=/fit-in/3072x2042/filters:format_auto():quality(85):upscale()/2019/04/18/769/n/1922283/eeb8651e5cb8b3908543d8.33607707_.jpg'
+  const RAY_IMAGE = 'https://www.slashfilm.com/img/gallery/can-the-return-of-rey-redeem-the-sins-of-star-wars-the-rise-of-skywalker/intro-1680978833.webp'
+  return (
+    <div className="App">
+      <Post 
+        author={{
+          name: "Anakin skywalker",
+          photo: ANAKIN_IMAGE,
+          nickname: "@dart_vader"
+          }}
+          content="WTF? Who is Ray? Why she is Skywalker? Luke...?"
+          image={RAY_IMAGE}
+          date={"26 лют."}
+      />
+      
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ homework16/homework16-app/src/post/Post.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import module from "./Post.module.css";
+const Post = ({ author, content, image, date }) => {
+  return (
+    <main>
+    <div className={module.post__container}>
+      <div className={module.img__container}>
+        <span className={module.img__span}>
+          <img src={author.photo} alt="Icon" />
+        </span>
+      </div>
+      <div className={module.content__data}>
+        <div className={module.data}>
+          <div className={module.author__data}>
+            <span>
+              {author.name}
+              <i
+                style={{ marginLeft: "5px" }}
+                className="fas fa-check-circle"
+              ></i>
+            </span>
+          </div>
+          <div className={module.author__data}>
+            <span className={module.span__data}>{author.nickname}</span>
+          </div>
+          <div className={module.author__data}>
+            <span className={module.span__data}>{date}</span>
+          </div>
+        </div>
+        <div className={module.content__text}>{content}</div>
+        <div className={module.content__image}><img src={image} alt="" /></div>
+        <div className={module.activity__content}>
+        <i class="fa-regular fa-heart"> <span style={{paddingLeft:"5px"}}>556</span> </i>
+        <i class="fa-solid fa-share"><span style={{paddingLeft:"5px"}}>127</span></i>
+        <i class="fa-regular fa-comment"><span style={{paddingLeft:"5px"}}>48</span></i>
+        <i class="fa-solid fa-download"><span style={{paddingLeft:"5px"}}>Завантажити</span></i>
+        </div>
+      </div>
+      
+    </div>
+    </main>
+  );
+};
+
+export default Post;
