ccd9f674bf050bf8a0f59df25f2de0404b84f0d5
Hallory <khalpeep@gmail.com>
2023-08-03 18:35:17 +0200

    added homework13

---
+++ homework13/main.js
@@ -0,0 +1,41 @@
+ function* createIdGenerator(){
+    let id = 1;
+    while (true) {
+        yield id;
+        id++;
+    }
+}
+const idGenerator = createIdGenerator();
+
+
+const upBtn = document.querySelector('.upBtn');
+const downBtn = document.querySelector('.downBtn');
+const container = document.querySelector('.container');
+
+function* newFornGenerator(startSize){
+    let fontSize = startSize;
+    while(true){
+    const direction = yield fontSize
+            if (direction === "up") {
+                fontSize+=2
+    }
+            else if(direction === "down"){
+                fontSize-=2
+    }
+    }
+    
+}
+
+const fontGenerator = newFornGenerator(14);
+
+upBtn.addEventListener('click', ()=>{
+    const newSize = fontGenerator.next("up").value;
+    container.style.fontSize = `${newSize}px`
+    console.log(newSize)
+})
+downBtn.addEventListener('click', ()=>{
+    const newSize = fontGenerator.next("down").value;
+    container.style.fontSize = `${newSize}px`
+    console.log(newSize)
+
+})
